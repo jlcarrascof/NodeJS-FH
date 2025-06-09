8a2791a3c6452a920c3ad3e0537e1cfc938e79df
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-06-08 22:44:14 -0400

    Replace occurrences of 'React' with 'Angular' in README.md and save as README-Angular.md

--- 01-Foundations/app2.js
+++ 01-Foundations/app2.js
@@ -2,5 +2,7 @@
 const fs = require('fs');
 
 const data = fs.readFileSync('README.md', 'utf8');
+const newData = data.replace(/React/ig, 'Angular');
 
+fs.writeFileSync('README-Angular.md', newData);
 console.log(data);
