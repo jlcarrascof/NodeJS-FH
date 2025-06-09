34b0c8f890b851c3515b65ad585e2e29ed85a9e5
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-06-08 22:45:59 -0400

    Update app2.js to log modified content instead of original data

--- 01-Foundations/app2.js
+++ 01-Foundations/app2.js
@@ -5,4 +5,4 @@ const data = fs.readFileSync('README.md', 'utf8');
 const newData = data.replace(/React/ig, 'Angular');
 
 fs.writeFileSync('README-Angular.md', newData);
-console.log(data);
+console.log(newData);
