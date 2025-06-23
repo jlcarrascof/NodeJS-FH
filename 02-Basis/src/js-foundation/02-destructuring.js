dce53d3d3841c45df716d1056cb5e481469718c9
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-06-23 08:30:25 -0400

    Add Green Lantern to characters array in 02-destructuring.js

--- 02-Basis/src/js-foundation/02-destructuring.js
+++ 02-Basis/src/js-foundation/02-destructuring.js
@@ -5,7 +5,7 @@ const { SHELL, HOMEBREW_PREFIX, npm_lifecycle_script } = process.env;
 
 // console.table({SHELL, HOMEBREW_PREFIX, npm_lifecycle_script});
 
-const characters = ['Flash', 'Superman', 'Batman'];
-const [ , , batman] = characters;
+const characters = ['Flash', 'Superman', 'Green Lantern', 'Batman'];
+const [ , , , batman] = characters;
 
 // console.log(batman);
