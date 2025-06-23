4e0aa7fad7c56db00f6737f3e9542ab9de64c9fd
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-06-22 22:08:57 -0400

    Use console.table for environment variable logging in 02-destructuring.js

--- 02-Basis/src/js-foundation/02-destructuring.js
+++ 02-Basis/src/js-foundation/02-destructuring.js
@@ -3,4 +3,4 @@ console.log( process.env.PORT ?? 3000 );
 
 const { SHELL, HOMEBREW_PREFIX, npm_lifecycle_script } = process.env;
 
-console.log({SHELL, HOMEBREW_PREFIX, npm_lifecycle_script});
+console.table({SHELL, HOMEBREW_PREFIX, npm_lifecycle_script});
