668a6c88eec2f88ae3599b7dae7ecda33d0491be
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-06-22 21:18:57 -0400

    Enhance environment variable logging by including HOMEBREW_PREFIX and npm_lifecycle_script in 02-destructuring.js

--- 02-Basis/src/js-foundation/02-destructuring.js
+++ 02-Basis/src/js-foundation/02-destructuring.js
@@ -1,6 +1,6 @@
 
 console.log( process.env.PORT ?? 3000 );
 
-const { SHELL } = process.env;
+const { SHELL, HOMEBREW_PREFIX, npm_lifecycle_script } = process.env;
 
-console.log(SHELL);
+console.log(SHELL, HOMEBREW_PREFIX, npm_lifecycle_script);
