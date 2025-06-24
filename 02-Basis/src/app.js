890d8bd859bc6e9c9c5e08e9e795cf679c946f86
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-06-23 21:03:08 -0400

    Comment out unused requires in app.js and add users array in callbacks.js

--- 02-Basis/src/app.js
+++ 02-Basis/src/app.js
@@ -1,7 +1,7 @@
 
-const {emailTemplate} = require ('./js-foundation/01-template');
-require('./js-foundation/02-destructuring');
+// const {emailTemplate} = require ('./js-foundation/01-template');
+// require('./js-foundation/02-destructuring');
+require('./js-foundation/03-callbacks');
 
-// console.log(emailTemplate);
 
 // console.log(emailTemplate);
