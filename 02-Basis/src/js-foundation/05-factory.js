69401097b1bea80b348e0b45fe51b56233bb8b60
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-06-25 21:21:03 -0400

    Implement buildPerson function to create a person object with id, name, birthdate, and age

--- 02-Basis/src/js-foundation/05-factory.js
+++ 02-Basis/src/js-foundation/05-factory.js
@@ -2,6 +2,11 @@ const obj = { name: 'Javier', birthdate: '1968-03-09' };
 
 const buildPerson = ( { name, birthdate } ) => {
 
-
+    return {
+        id: new Date().getTime(),
+        name: name,
+        birthdate: birthdate,
+        age: new Date().getFullYear() - new Date(birthdate).getFullYear()
+    };
 
 }
