369a2247be7d2e9567b3913ed9c822ce2f8c0210
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-06-25 22:37:46 -0400

    Refactor age calculation in buildPerson function to use get-age module

--- 02-Basis/src/js-foundation/05-factory.js
+++ 02-Basis/src/js-foundation/05-factory.js
@@ -1,11 +1,12 @@
 const { v4: uuidv4 } = require('uuid');
+const getAge = require('get-age');
 
 const buildPerson = ( { name, birthdate } ) => {
     return {
         id: uuidv4(),
         name: name,
         birthdate: birthdate,
-        age: new Date().getFullYear() - new Date(birthdate).getFullYear()
+        age: getAge( birthdate ),
     };
 }
 
