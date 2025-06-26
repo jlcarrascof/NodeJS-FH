2b26692e46ad6d6b1cc95c015afee3b00012b647
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-06-25 22:35:04 -0400

    Replace timestamp with UUID for person ID generation in buildPerson function

--- 02-Basis/src/js-foundation/05-factory.js
+++ 02-Basis/src/js-foundation/05-factory.js
@@ -1,6 +1,8 @@
+const { v4: uuidv4 } = require('uuid');
+
 const buildPerson = ( { name, birthdate } ) => {
     return {
-        id: new Date().getTime(),
+        id: uuidv4(),
         name: name,
         birthdate: birthdate,
         age: new Date().getFullYear() - new Date(birthdate).getFullYear()
