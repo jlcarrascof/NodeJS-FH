fa14eb056ad38c6475936aa0c35040128975f102
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-06-30 11:04:06 -0400

    Update getPokemonById to accept a callback and modify app.js to log Pokémon data using the callback

--- 02-Basis/src/app.js
+++ 02-Basis/src/app.js
@@ -6,8 +6,9 @@ const { getUUID, getAge } = require('./plugins');
 const getPokemonById = require('./js-foundation/06-promises');
 
 
-const name = getPokemonById( 4 );
-console.log( { name } );
+getPokemonById( 4, ( pokemon ) => {
+    console.log({ pokemon });
+});
 
 
 // ! Reference to Factory Pattern
--- 02-Basis/src/js-foundation/06-promises.js
+++ 02-Basis/src/js-foundation/06-promises.js
@@ -1,13 +1,13 @@
 
 
-const getPokemonById = ( id ) => {
+const getPokemonById = ( id, callback ) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${ id }`;
 
     fetch( url ).then( ( response ) => {
         response.json().then( ( pokemon ) => {
             // console.log( pokemon.name );
 
-            return pokemon.name;
+           callback( pokemon.name );
         });
     });
 
