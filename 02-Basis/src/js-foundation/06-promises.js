20cd60d67c0e094ad266b474c490e6f3a1f6f3ea
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-06-30 17:30:26 -0400

    Refactor getPokemonById to streamline fetch logic and remove commented code

--- 02-Basis/src/js-foundation/06-promises.js
+++ 02-Basis/src/js-foundation/06-promises.js
@@ -3,13 +3,19 @@
 const getPokemonById = ( id, callback ) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${ id }`;
 
-    fetch( url ).then( ( response ) => {
-        response.json().then( ( pokemon ) => {
-            // console.log( pokemon.name );
-
-           callback( pokemon.name );
+    fetch( url )
+        .then( ( resp ) => resp.json())
+        .then( ( pokemon ) => {
+            callback( pokemon.name );
         });
-    });
+
+    // fetch( url ).then( ( response ) => {
+    //     response.json().then( ( pokemon ) => {
+    //         // console.log( pokemon.name );
+
+    //        callback( pokemon.name );
+    //     });
+    // });
 
 
     // return 'Pokemon';
