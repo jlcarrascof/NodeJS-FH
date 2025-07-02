824af09b972e61bb40f15d260da60df8c7aff7ea
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-07-01 21:48:37 -0400

    Refactor getPokemonById to use http client for API requests and update plugin exports

--- 02-Basis/src/js-foundation/06-promises.js
+++ 02-Basis/src/js-foundation/06-promises.js
@@ -1,9 +1,11 @@
-
+const { http } = require('../plugins');
 
 const getPokemonById = async ( id ) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${ id }`;
-    const resp = await fetch( url );
-    const pokemon = await resp.json();
+    const pokemon = await http.get( url );
+
+    // const resp = await fetch( url );
+    // const pokemon = await resp.json();
 
     // throw new Error('Pokemon not found');
 
--- 02-Basis/src/plugins/http-client.plugin.js
+++ 02-Basis/src/plugins/http-client.plugin.js
@@ -1,11 +1,19 @@
 
 const httpClientPlugin = {
 
+    get: async(url) => {
+        const resp = await fetch( url );
+        return await resp.json();
+    },
+
+    post: async(url, body) => {},
+    put: async(url, body) => {},
+    delete: async(url) => {},
 };
 
 
 
 
 module.exports = {
-    httpClientPlugin,
+    http: httpClientPlugin,
 };
