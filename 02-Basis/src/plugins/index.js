4bff3a2e4906751e694c7e269ee310e79995eb6d
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-07-03 22:52:40 -0400

    Add combined.log and error.log files; update index.js to export buildLogger

--- 02-Basis/src/plugins/index.js
+++ 02-Basis/src/plugins/index.js
@@ -1,9 +1,12 @@
 const { getUUID } = require('../plugins/get-id.plugin');
 const { getAge } = require('../plugins/get-age.plugin');
+
 const { http } = require('../plugins/http-client.plugin');
+const buildLogger = require('../plugins/logger.plugin');
 
 module.exports = {
     getUUID,
     getAge,
     http,
+    buildLogger,
 };
