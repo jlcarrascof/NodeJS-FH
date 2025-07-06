6ddb412929aa119f9d4178ca7f4997e7d98d41ff
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-07-06 12:57:41 -0400

    Add heroes data structure to app.js

---
+++ 03-TypeScript/src/app.js
@@ -0,0 +1,17 @@
+const heroes = [
+    {
+        id : 1,
+        name: 'Ironman',
+        owner: 'Marvel',
+    },
+    {
+        id : 2,
+        name: 'Spiderman',
+        owner: 'Marvel',
+    },
+    {
+        id : 3,
+        name: 'Batman',
+        owner: 'DC',
+    },
+];
